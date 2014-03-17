d33cdc34466dabd4eb2040cfb8e9e155740cf190
Hsun <hsun@hsun-BM5242-BM5342-BM5642.(none)>
2014-03-17 14:46:10 +0800

    kd

--- two_wheel_balancing/main.c
+++ two_wheel_balancing/main.c
@@ -72,37 +72,6 @@ void init_tim2() //timer for time delay
 	TIM_ITConfig( TIM2, TIM_IT_Update, ENABLE);
 	TIM_Cmd(TIM2, ENABLE);
 }
-void init_tim3() //timer for contral
-{	
-	RCC_PCLK1Config(RCC_HCLK_Div2);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-
-	NVIC_InitTypeDef NVIC_InitStructure;
-
-	/* Enable the TIM2 global Interrupt */
-	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-
-	NVIC_Init(&NVIC_InitStructure);
-
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-	/* Time base configuration */
-	TIM_TimeBaseStructure.TIM_Period = 7200-1;
-	TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t)(100-1);
-
-	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-
-	// /* Prescaler configuration */
-	// TIM_PrescalerConfig(TIM2, PrescalerValue, TIM_PSCReloadMode_Immediate);
-
-	TIM_ITConfig( TIM3, TIM_IT_Update, ENABLE);
-	TIM_Cmd(TIM3, ENABLE);
-}
 
 void init_tim4_pwm()
 {	
